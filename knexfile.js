eaf20ed35b49921c90f113c7f648a31cb86f8d21
Bhargav Sai <bhargavsai@Bhargavs-MacBook-Air.local>
2026-05-31 19:17:25 -0400

    fix: use JAWSDB_URL for production database

--- knexfile.js
+++ knexfile.js
@@ -12,13 +12,7 @@ module.exports = {
   },
   production: {
     client: 'mysql2',
-    connection: process.env.JAWSDB_URL || {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT || 3306),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-    },
+    connection: process.env.JAWSDB_URL,
     pool: { min: 2, max: 10 },
     migrations: { directory: './dist/migrations' }
   }
