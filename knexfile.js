05a3135b5c4563cf2aaf732e623d0ed1b64fd8a2
Bhargav Sai <bhargavsai@Bhargavs-MacBook-Air.local>
2026-05-31 19:23:15 -0400

    fix: fix knexfile exports

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,21 @@
 require('dotenv').config();
-module.exports = {
-  development: {
-    client: 'mysql2',
-    connection: {
-      socketPath: process.env.DB_SOCKET || '/tmp/mysql.sock',
-      user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME || 'lendsqr_wallet'
-    },
-    migrations: { directory: './dist/migrations' }
-  },
-  production: {
-    client: 'mysql2',
-    connection: process.env.JAWSDB_URL,
-    pool: { min: 2, max: 10 },
-    migrations: { directory: './dist/migrations' }
-  }
+
+const production = {
+  client: 'mysql2',
+  connection: process.env.JAWSDB_URL,
+  pool: { min: 2, max: 10 },
+  migrations: { directory: './dist/migrations' }
 };
+
+const development = {
+  client: 'mysql2',
+  connection: {
+    socketPath: process.env.DB_SOCKET || '/tmp/mysql.sock',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'lendsqr_wallet'
+  },
+  migrations: { directory: './dist/migrations' }
+};
+
+module.exports = { development, production, test: development };
